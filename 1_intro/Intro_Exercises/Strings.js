a3d8f3034e36475b9812c1b36ead3a642aeed2ac
Joakim Thorum Poulsen <jp@boblberg.dk>
2020-02-06 10:48:51 +0100

    Exercise 6 & 7

--- 1_intro/Intro_Exercises/Strings.js
+++ 1_intro/Intro_Exercises/Strings.js
@@ -4,9 +4,9 @@
 var numberOne = "1.10";
 var numberTwo = "2.30";
 
-var addNumbers = parseFloat(numberOne) + parseFloat(numberTwo)
+var addNumbers = parseFloat(numberOne) + parseFloat(numberTwo);
 
-console.log(addNumbers)
+console.log(addNumbers);
 
 // add those two numbers and show the result
 // you cannot touch line 1 neither line 2
@@ -20,9 +20,9 @@ console.log(addNumbers)
 var numberOne = "1.10";
 var numberTwo = "2.30";
 
-var decimals = addNumbers.toFixed(2)
+var decimals = addNumbers.toFixed(2);
 
-console.log(decimals)
+console.log(decimals);
 
 // --------------------------------------
 // Exercise 5 - Decimals and average
@@ -33,10 +33,34 @@ var three = 98;
 
 var summed = (one + two + three /3);
 
-var avg = summed.toFixed(5)
+var avg = summed.toFixed(5);
 
-console.log(avg)
+console.log(avg);
 
 // Show in the console the avg. with 5 decimals
+
+// --------------------------------------
+
+// --------------------------------------
+// Exercise 6 - Get the character by index
+
+var letters = "abc";
+// Get me the character "c"
+
+var letter = letters.charAt(2);
+
+console.log(letter);
+
+
+// --------------------------------------
+// Exercise 7 - Replace
+
+var fact = "You are learning javascript!";
+
+var newFact = fact.replace("j", "J");
+
+console.log(newFact);
+
+// capitalize the J in Javascript
 
 // --------------------------------------
